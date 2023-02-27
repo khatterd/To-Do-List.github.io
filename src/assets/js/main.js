772e2d3e21555688c8e6003f4289d435c3b05277
khatterd <deveshkhatter@gmail.com>
2023-02-27 07:32:40 -0500

    Styling Complete

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -27,6 +27,7 @@ function addtoList()
     }
     else
     {
+        task = JSON.parse(localStorage.getItem("task")) || [];
         task.push(taskValue);
         localStorage.setItem("task", JSON.stringify(task))
         displayTasks();
@@ -38,23 +39,32 @@ function displayTasks()
 {
     tasklist = JSON.parse(localStorage.getItem("task"));
     var list = document.querySelector('#listBox')
+    var taskbox = document.querySelector('.taskbox')
     
     if (tasklist.length == 0)
     {   
-        {
-            list.innerHTML = "No Tasks Added";
-            document.getElementById('clearbox').innerHTML ="" 
-        }
+
+        taskbox.classList.add('centre')
+        list.innerHTML = `<h4 class="text-light bg-dark p-3">No Tasks Added</h4>`;
+        document.getElementById('clearbox').innerHTML ="" 
+    
     }
     else
     {
+        taskbox.classList.remove('centre')
         document.getElementById('clearbox').innerHTML = 
         `<button class="btn btn-danger" onclick="clearTask()">Clear All</button>`
 
         list.innerHTML = ""
         for (let [index, value] of tasklist.entries())
         {
-            list.innerHTML += `<li class="list-group-item ">${value} <img onclick = "editTask(${index})" src = "src/assets/icons/pencil-square.svg"> <img onclick = "deleteTask(${index})" src = "src/assets/icons/x-square.svg"></li>`;
+            list.innerHTML += `
+            <li class="list-group-item list-group-item-light d-flex justify-content-between align-items-center">
+              <span class="me-auto ps-2">${value}</span>
+              <img onclick="editTask(${index})" src="src/assets/icons/pencil-square.svg" class="ms-2 me-1 float-end">
+              <img onclick="deleteTask(${index})" src="src/assets/icons/x-square.svg" class="me-2 float-end">
+            </li>
+          `;
         }
     }
 }
